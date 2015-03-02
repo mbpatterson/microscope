2aab1b4166eb4b6f7ef99b3da942319f00111033
Michael Patterson <michaelpatterson22@gmail.com>
2015-03-01 21:14:53 -0500

    4-3 wired collection into postsList template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://telescope.com'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'Meteor Book',
-    url: 'http://discovermeteor.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
