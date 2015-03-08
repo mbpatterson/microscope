5bc3ed2f5344134bee6369b824014896810dd9b9
Michael Patterson <michaelpatterson22@gmail.com>
2015-03-08 16:43:52 -0400

    10-1 added comments collection, pub/sub and fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,38 @@
+//fixture data
 if (Posts.find().count() === 0) {
-  Posts.insert({
+  var now = new Date().getTime();
+
+  //create two users
+  var tomId = Meteor.users.insert({
+    profile: {name: 'Tom Coleman'}
+  });
+  var tom = Meteor.users.findOne(tomId);
+  var sachaId = Meteor.users.insert({
+    profile: {name: 'Sacha Greif' }
+  });
+  var sacha = Meteor.users.findOne(sachaId);
+
+  var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
-    url: 'http://telescope.com'
+    userId: sacha._id,
+    author: sacha.profile.name,
+    url: 'http://telescope.com',
+    submitted: new Date(now -7 * 3600 * 1000)
   });
 
-  Posts.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
+  Comments.insert({
+    postId: telescopeId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Interesting project!'
   });
 
-  Posts.insert({
-    title: 'Meteor Book',
-    url: 'http://discovermeteor.com'
+  Comments.insert({
+    postId: telescopeId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'Aw yea!'
   });
 }
